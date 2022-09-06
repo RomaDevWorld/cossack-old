ca6073a3f1d55d0ec8319ee37dbfd7eb3fa283b7
RomaPlayWorld <romantarasyk6615@gmail.com>
2022-09-06 16:52:27 +0300

    Ignoring some files

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js')
+const { QuickDB } = require("quick.db");
+const db = new QuickDB()
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Відображає затримку між хостингом і серверами Discord'),
     async execute(interaction) {  
+        await db.push(`pin_${interaction.user.id}`, `${Date.now()}`);
         await interaction.reply(`${interaction.client.ws.ping}ms`)
+
+        console.log(await db.get(`pin_${interaction.user.id}`))
     }
 }
